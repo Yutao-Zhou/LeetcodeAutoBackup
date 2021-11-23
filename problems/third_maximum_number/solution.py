76b8da92beb66d1cb4876e638764ea417d14390f
Yutao-Zhou <13520759678@163.com>
2021-11-23 03:48:21 +0000

    Sync LeetCode submission - Third Maximum Number (python3)

--- problems/third_maximum_number/solution.py
+++ problems/third_maximum_number/solution.py
@@ -1,29 +1,48 @@
+   
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         ###### bad solution ####
-#         cach1 = 0
-#         cach2 = 0
-#         cach3 = 0
-#         for i in nums:
-#             print(cach1,cach2,cach3)
-#             if i > cach1:
-#                 cach1 = i
-#             elif i > cach2:
-#                 cach2 = i
-#             elif i > cach3:
-#                 cach3 = i
-        
-#         if len(set(nums)) < 3:
-#             return cach1
-#         else:
-#             return cach3
-        
-        ###### set #######
-        nums = set(nums)
-        nums = list(nums)
-        nums.sort()
-        print(nums)
-        if len(nums) < 3:
-            return nums[-1]
+        cach = [-2 ** 33,-2 ** 33,-2 ** 33]
+        for i in nums:
+            if i != cach[0] and i != cach[1] and i != cach[2]:
+                if i > cach[0]:
+                    cach.insert(0,i)
+                    cach.pop()
+                elif i > cach[1]:
+                    cach.insert(1,i)
+                    cach.pop()
+                elif i > cach[2]:
+                    cach.insert(2,i)
+                    cach.pop()
+        if len(set(nums)) < 3:
+            return cach[0]
         else:
-            return nums[-3]
+            return cach[2]
+        
+        ###### set and sort not doing the problem#######
+        # nums = set(nums)
+        # nums = list(nums)
+        # nums.sort()
+        # print(nums)
+        # if len(nums) < 3:
+        #     return nums[-1]
+        # else:
+        #     return nums[-3]
+        
+        ##### bubble sort algorism #######
+        # for i in range(len(nums)):
+        #     already_sorted = True
+        #     for j in range(len(nums) - i - 1):
+        #         if nums[j] > nums[j + 1]:
+        #             nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        #             already_sorted = False
+        #     if already_sorted:
+        #         break
+        # ck = []
+        # for i in range(-1,-len(nums)-1,-1):
+        #     if nums[i] not in ck:
+        #         ck.append(nums[i])
+        # if len(ck) < 3:
+        #     return ck[0]
+        # else:
+        #     return ck[2]
